0bf8f973c533f89b44b948722ec202ce46a22bbd
zhangjian95 <1137473746@qq.com>
2017-05-10 16:51:07 +0800

    新小嘉

--- src/git_04/Git4.java
+++ src/git_04/Git4.java
@@ -3,5 +3,9 @@ package git_04;
 public class Git4 {
 	public static void main(String[] args) {
 		System.out.println("git04");
+		
+	}
+	public void hah() {
+		System.out.println("ห๗เหฃกฃก");
 	}
 }
